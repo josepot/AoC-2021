16e6ab2397820fb21e2cbfaad130509268688a9d
Josep M Sobrepere <jm.sobrepere@gmail.com>
2021-12-02 06:22:49 +0100

    Day 2 cleanup

--- src/2/solution.ts
+++ src/2/solution.ts
@@ -1,46 +1,49 @@
-const solution1 = (lines: string[]) => {
-  const instructions = lines.map((line) => {
-    const result = line.split(" ")
-    return [result[0], Number(result[1])] as const
+const readInstructions = (lines: string[]) =>
+  lines.map((line) => {
+    const [instruction, deltaStr] = line.split(" ") as [
+      "forward" | "down" | "up",
+      string,
+    ]
+    return [instruction, Number(deltaStr)] as const
   })
 
+const solution1 = (lines: string[]) => {
   let x = 0
   let y = 0
-  instructions.forEach(([command, delta]) => {
-    if (command === "forward") {
-      x += delta
-    }
-    if (command === "down") {
-      y += delta
-    }
-    if (command === "up") {
-      y -= delta
+
+  readInstructions(lines).forEach(([command, delta]) => {
+    switch (command) {
+      case "forward":
+        return (x += delta)
+      case "down":
+        return (y += delta)
+      case "up":
+        return (y -= delta)
     }
   })
+
   return x * y
 }
 
 const solution2 = (lines: string[]) => {
-  const instructions = lines.map((line) => {
-    const result = line.split(" ")
-    return [result[0], Number(result[1])] as const
-  })
-
   let x = 0
   let y = 0
   let aim = 0
-  instructions.forEach(([command, delta]) => {
-    if (command === "forward") {
-      x += delta
-      y += aim * delta
-    }
-    if (command === "down") {
-      aim += delta
-    }
-    if (command === "up") {
-      aim -= delta
+
+  readInstructions(lines).forEach(([command, delta]) => {
+    switch (command) {
+      case "forward": {
+        x += delta
+        y += aim * delta
+        break
+      }
+      case "down":
+        return (aim += delta)
+      case "up":
+        return (aim -= delta)
     }
   })
+
   return x * y
 }
 
