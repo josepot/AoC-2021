87b5ab96eb9b128901368b014bb8a7d52fc0c22c
Josep M Sobrepere <jm.sobrepere@gmail.com>
2021-12-06 21:15:47 +0100

    Day 6 cleanup

--- src/6/solution.ts
+++ src/6/solution.ts
@@ -1,24 +1,19 @@
 import add from "utils/add"
 
 const solution1 = (line: string, nDays = 80) => {
-  let fish = new Map<number, number>()
-  for (let i = 0; i < 9; i++) fish.set(i, 0)
+  let fish = Array(9).fill(0)
 
   line
     .split(",")
     .map(Number)
     .forEach((value) => {
-      fish.set(value, fish.get(value)! + 1)
+      fish[value]++
     })
 
   for (let i = 0; i < nDays; i++) {
-    fish.set(9, fish.get(0)!)
-    fish = new Map(
-      [...fish.entries()].map(([key, value]) =>
-        key === 7 ? [6, value + (fish.get(9) ?? 0)] : [key - 1, value],
-      ),
+    fish = fish.map((_, key) =>
+      key === 6 ? fish[7] + fish[0] : fish[(key + 1) % 9],
     )
-    fish.delete(-1)
   }
 
   return [...fish.values()].reduce(add)
