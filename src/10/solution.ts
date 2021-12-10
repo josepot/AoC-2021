af775201f2fcb366ab95fd4a0c089527200f3ee0
Josep M Sobrepere <jm.sobrepere@gmail.com>
2021-12-10 22:01:19 +0100

    Day 10 cleanup a bit

--- src/10/solution.ts
+++ src/10/solution.ts
@@ -22,19 +22,20 @@ const completionPoints: Record<string, number> = {
   ">": 4,
 }
 
-const getScore = (line: string): number | null => {
-  const chars = line.split("")
+// zero means that it's leagal
+// greater than zero is the points for corrupted lines
+// smaller than zero are the negated points for incomplete lines
+const getScore = (chars: string): number => {
   const stack = new Stack<string>()
-  let i = 0
-  for (; i < chars.length; i++) {
+  for (let i = 0; i < chars.length; i++) {
     const char = chars[i]
     const closing = openToClose[char]
     if (closing) {
       stack.push(closing)
-    } else {
-      if (stack.peek() !== char) return points[char]
-      stack.pop()
+      continue
     }
+    if (stack.peek() !== char) return points[char]
+    stack.pop()
   }
 
   let score = 0
@@ -48,13 +49,13 @@ const getScore = (line: string): number | null => {
 const solution1 = (lines: string[]) =>
   lines
     .map(getScore)
-    .filter((x): x is number => x !== null && x > 0)
+    .filter((x) => x > 0)
     .reduce(add)
 
 const solution2 = (lines: string[]) => {
   const sortedResults = lines
     .map(getScore)
-    .filter((x): x is number => x !== null && x < 0)
+    .filter((x) => x < 0)
     .sort((a, b) => a - b)
 
   return sortedResults[(sortedResults.length - 1) / 2] * -1
