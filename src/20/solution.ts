1aaa4dad2a785f4a664500528be71118114f6818
Josep M Sobrepere Profitós <josepot@gmail.com>
2021-12-20 00:24:11 -0700

    Day 20

--- src/20/solution.ts
+++ src/20/solution.ts
@@ -1,30 +1,29 @@
-const solution1 = (lines: string[]) => {
+const solution1 = (lines: string[], nTimes = 2) => {
   const base = lines[0]
   let baseImage = lines.slice(2)
 
-  const getPixelAtPos = (x: number, y: number) => {
+  const getPixelAtPos = (x: number, y: number, idx: number) => {
+    const defaultC = idx % 2 === 0 ? "." : "#"
     let binaryStr = ""
 
-    binaryStr += (baseImage[y - 1]?.[x - 1] ?? ".") === "." ? "0" : "1"
-    binaryStr += (baseImage[y - 1]?.[x] ?? ".") === "." ? "0" : "1"
-    binaryStr += (baseImage[y - 1]?.[x + 1] ?? ".") === "." ? "0" : "1"
+    binaryStr += (baseImage[y - 1]?.[x - 1] ?? defaultC) === "." ? "0" : "1"
+    binaryStr += (baseImage[y - 1]?.[x] ?? defaultC) === "." ? "0" : "1"
+    binaryStr += (baseImage[y - 1]?.[x + 1] ?? defaultC) === "." ? "0" : "1"
 
-    binaryStr += (baseImage[y]?.[x - 1] ?? ".") === "." ? "0" : "1"
-    binaryStr += (baseImage[y]?.[x] ?? ".") === "." ? "0" : "1"
-    binaryStr += (baseImage[y]?.[x + 1] ?? ".") === "." ? "0" : "1"
+    binaryStr += (baseImage[y]?.[x - 1] ?? defaultC) === "." ? "0" : "1"
+    binaryStr += (baseImage[y]?.[x] ?? defaultC) === "." ? "0" : "1"
+    binaryStr += (baseImage[y]?.[x + 1] ?? defaultC) === "." ? "0" : "1"
 
-    binaryStr += (baseImage[y + 1]?.[x - 1] ?? ".") === "." ? "0" : "1"
-    binaryStr += (baseImage[y + 1]?.[x] ?? ".") === "." ? "0" : "1"
-    binaryStr += (baseImage[y + 1]?.[x + 1] ?? ".") === "." ? "0" : "1"
+    binaryStr += (baseImage[y + 1]?.[x - 1] ?? defaultC) === "." ? "0" : "1"
+    binaryStr += (baseImage[y + 1]?.[x] ?? defaultC) === "." ? "0" : "1"
+    binaryStr += (baseImage[y + 1]?.[x + 1] ?? defaultC) === "." ? "0" : "1"
 
-    const idx = parseInt(binaryStr, 2)
-
-    return base[idx]
+    return base[parseInt(binaryStr, 2)]
   }
 
-  const margin = 10
+  const margin = 1
 
-  for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < nTimes; i++) {
     baseImage = Array(baseImage.length + margin + margin)
       .fill(null)
       .map((_, idx) => idx - margin)
@@ -32,25 +31,21 @@ const solution1 = (lines: string[]) => {
         Array(baseImage[0].length + margin + margin)
           .fill(null)
           .map((_, idx) => idx - margin)
-          .map((x) => getPixelAtPos(x, y))
+          .map((x) => getPixelAtPos(x, y, i))
           .join(""),
       )
   }
 
   return baseImage
-    .slice(18)
-    .slice(0, -18)
     .map((line) =>
       line
         .split("")
-        .slice(18)
-        .slice(0, -18)
         .filter((x) => x === "#")
         .join(""),
     )
     .join("").length
 }
 
-const solution2 = (lines: string[]) => {}
+const solution2 = (lines: string[]) => solution1(lines, 50)
 
-export default [solution1]
+export default [solution1, solution2]
