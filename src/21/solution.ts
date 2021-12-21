8caff26815b61ccca17cb353bc52f23fb06d327f
Josep M Sobrepere Profitós <josepot@gmail.com>
2021-12-20 23:41:29 -0700

    Day 21 part 2

--- src/21/solution.ts
+++ src/21/solution.ts
@@ -23,6 +23,61 @@ const solution1 = (lines: string[]) => {
   return scores[turn] * nRolls
 }
 
-const solution2 = (lines: string[]) => {}
+const dies: Array<number> = []
+for (let i = 1; i < 4; i++) {
+  for (let z = 1; z < 4; z++) {
+    for (let y = 1; y < 4; y++) {
+      dies.push(i + z + y)
+    }
+  }
+}
 
-export default [solution1]
+const memo: Record<string, [number, number]> = {}
+const setMemo = (
+  positions: [number, number],
+  scores: [number, number],
+  turn: number,
+  result: [number, number],
+) => {
+  memo[positions.join(",") + scores.join(",") + turn] = result
+}
+
+const getMemo = (
+  positions: [number, number],
+  scores: [number, number],
+  turn: number,
+): [number, number] | undefined =>
+  memo[positions.join(",") + scores.join(",") + turn]
+
+const play = (
+  positions: [number, number],
+  scores: [number, number],
+  turn: number,
+): [number, number] => {
+  const memoed = getMemo(positions, scores, turn)
+  if (memoed) return memoed
+
+  if (scores[0] >= 21) return [1, 0]
+  if (scores[1] >= 21) return [0, 1]
+
+  const results = dies.map((roll) => {
+    const nextPositions: [number, number] = [...positions]
+    const nextScores: [number, number] = [...scores]
+    nextPositions[turn] = (positions[turn] + roll) % 10
+    nextScores[turn] += nextPositions[turn] + 1
+    return play(nextPositions, nextScores, turn === 0 ? 1 : 0)
+  })
+
+  const result: [number, number] = [0, 0]
+  results.forEach(([a, b]) => {
+    result[0] += a
+    result[1] += b
+  })
+  setMemo(positions, scores, turn, result)
+
+  return result
+}
+
+const solution2 = () => Math.max(...play([7, 0], [0, 0], 0))
+
+export default [solution1, solution2]
