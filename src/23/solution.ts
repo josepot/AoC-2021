f97ffd8f7d1a08eda718029e4e904c0a0d258d50
Josep M Sobrepere Profitós <josepot@gmail.com>
2021-12-23 10:05:59 -0700

    Day 23 part 2

--- src/23/solution.ts
+++ src/23/solution.ts
@@ -19,30 +19,6 @@ const codeAmphipod: Record<string, AmphipodType> = {
   C: AmphipodType.Cooper,
   D: AmphipodType.Desert,
 }
-const input = "CBBDDAAC"
-const input2 = input.split("").map((c) => codeAmphipod[c])
-
-const initialmap: Record<string, null | AmphipodType> = {}
-for (let x = 0; x < 11; x++) initialmap[`${x},0`] = null
-initialmap["2,1"] = input2[0]
-initialmap["2,2"] = input2[1]
-initialmap["4,1"] = input2[2]
-initialmap["4,2"] = input2[3]
-initialmap["6,1"] = input2[4]
-initialmap["6,2"] = input2[5]
-initialmap["8,1"] = input2[6]
-initialmap["8,2"] = input2[7]
-
-const initialAmphipods = [
-  { type: input2[0], state: State.Init, location: "2,1" },
-  { type: input2[1], state: State.Init, location: "2,2" },
-  { type: input2[2], state: State.Init, location: "4,1" },
-  { type: input2[3], state: State.Init, location: "4,2" },
-  { type: input2[4], state: State.Init, location: "6,1" },
-  { type: input2[5], state: State.Init, location: "6,2" },
-  { type: input2[6], state: State.Init, location: "8,1" },
-  { type: input2[7], state: State.Init, location: "8,2" },
-]
 
 const amphipodRows: Record<AmphipodType, number> = {
   [AmphipodType.Amber]: 2,
@@ -70,43 +46,123 @@ interface GamePosition {
 const getGamePositionId = (amphipods: Amphipod[]) =>
   amphipods.map((a) => a.location).join(",")
 
-const initialGamePosition: GamePosition = {
-  id: getGamePositionId(initialAmphipods),
-  prev: null,
-  points: 0,
-  map: initialmap,
-  amphipods: initialAmphipods,
-}
+const generateMoves =
+  (nSpots: number) =>
+  (currentGame: GamePosition): GamePosition[] | true => {
+    const { amphipods, points, map } = currentGame
+    if (amphipods.every((a) => a.state === State.Done)) return true
 
-const generateMoves = (currentGame: GamePosition): GamePosition[] | true => {
-  const { amphipods, points, map } = currentGame
-  if (amphipods.every((a) => a.state === State.Done)) return true
+    const result: GamePosition[] = []
 
-  const result: GamePosition[] = []
+    amphipods.forEach((amphipod) => {
+      if (amphipod.state === State.Done) return
 
-  amphipods.forEach((amphipod) => {
-    if (amphipod.state === State.Done) return
+      const [currentX, currentY] = amphipod.location.split(",").map(Number)
 
-    const [currentX, currentY] = amphipod.location.split(",").map(Number)
+      let initialPoints = points
+      if (amphipod.state === State.Init) {
+        if (map[`${currentX},${currentY - 1}`] !== null) return
 
-    let initialPoints = points
-    if (amphipod.state === State.Init) {
-      if (map[`${currentX},${currentY - 1}`] !== null) return
+        initialPoints += amphipod.type * currentY
 
-      initialPoints += amphipod.type * currentY
+        let nextPoints = initialPoints
 
-      let nextPoints = initialPoints
+        for (let x = currentX - 1; map[`${x},0`] === null; x--) {
+          nextPoints += amphipod.type
+          if (doorRows.has(x)) continue
 
-      for (let x = currentX - 1; map[`${x},0`] === null; x--) {
-        nextPoints += amphipod.type
-        if (doorRows.has(x)) continue
+          const nextAmphipods: Array<Amphipod> = amphipods.map((a) =>
+            a === amphipod
+              ? {
+                  ...a,
+                  state: State.Transit,
+                  location: `${x},0`,
+                }
+              : a,
+          )
+
+          const nextMap: Record<string, null | AmphipodType> = {
+            ...map,
+            [amphipod.location]: null,
+            [`${x},0`]: amphipod.type,
+          }
+
+          result.push({
+            id: getGamePositionId(nextAmphipods),
+            prev: currentGame,
+            points: nextPoints,
+            amphipods: nextAmphipods,
+            map: nextMap,
+          })
+        }
+
+        nextPoints = initialPoints
+        for (let x = currentX + 1; map[`${x},0`] === null; x++) {
+          nextPoints += amphipod.type
+          if (doorRows.has(x)) continue
+
+          const nextAmphipods: Array<Amphipod> = amphipods.map((a) =>
+            a === amphipod
+              ? {
+                  ...a,
+                  state: State.Transit,
+                  location: `${x},0`,
+                }
+              : a,
+          )
+
+          const nextMap: Record<string, null | AmphipodType> = {
+            ...map,
+            [amphipod.location]: null,
+            [`${x},0`]: amphipod.type,
+          }
+
+          result.push({
+            id: getGamePositionId(nextAmphipods),
+            prev: currentGame,
+            points: nextPoints,
+            amphipods: nextAmphipods,
+            map: nextMap,
+          })
+        }
+      }
+
+      const targetX = amphipodRows[amphipod.type]
+      let home: string | null = null
+      let homeLevel = 0
+      for (let i = 1; i <= nSpots; i++) {
+        const currentPos = `${targetX},${i}`
+        const currentVal = map[`${targetX},${i}`]
+        if (currentVal === null) {
+          home = currentPos
+          homeLevel = i
+          continue
+        }
+        if (currentVal !== amphipod.type) {
+          home = null
+        }
+        break
+      }
+
+      if (home) {
+        const direction = currentX > amphipodRows[amphipod.type] ? -1 : 1
+        let nextPoints = initialPoints
+
+        let x = currentX
+        do {
+          x += direction
+          nextPoints += amphipod.type
+          if (map[`${x},0`] !== null) return
+        } while (x !== targetX)
+
+        nextPoints += amphipod.type * homeLevel
 
         const nextAmphipods: Array<Amphipod> = amphipods.map((a) =>
           a === amphipod
             ? {
                 ...a,
-                state: State.Transit,
-                location: `${x},0`,
+                state: State.Done,
+                location: home!,
               }
             : a,
         )
@@ -114,7 +170,7 @@ const generateMoves = (currentGame: GamePosition): GamePosition[] | true => {
         const nextMap: Record<string, null | AmphipodType> = {
           ...map,
           [amphipod.location]: null,
-          [`${x},0`]: amphipod.type,
+          [home]: amphipod.type,
         }
 
         result.push({
@@ -125,87 +181,10 @@ const generateMoves = (currentGame: GamePosition): GamePosition[] | true => {
           map: nextMap,
         })
       }
+    })
 
-      nextPoints = initialPoints
-      for (let x = currentX + 1; map[`${x},0`] === null; x++) {
-        nextPoints += amphipod.type
-        if (doorRows.has(x)) continue
-
-        const nextAmphipods: Array<Amphipod> = amphipods.map((a) =>
-          a === amphipod
-            ? {
-                ...a,
-                state: State.Transit,
-                location: `${x},0`,
-              }
-            : a,
-        )
-
-        const nextMap: Record<string, null | AmphipodType> = {
-          ...map,
-          [amphipod.location]: null,
-          [`${x},0`]: amphipod.type,
-        }
-
-        result.push({
-          id: getGamePositionId(nextAmphipods),
-          prev: currentGame,
-          points: nextPoints,
-          amphipods: nextAmphipods,
-          map: nextMap,
-        })
-      }
-    }
-
-    const targetX = amphipodRows[amphipod.type]
-    const homeSpots = [`${targetX},1`, `${targetX},2`]
-    const home =
-      map[homeSpots[1]] === null
-        ? homeSpots[1]
-        : map[homeSpots[1]] === amphipod.type && map[homeSpots[0]] === null
-        ? homeSpots[0]
-        : null
-
-    if (home) {
-      const direction = currentX > amphipodRows[amphipod.type] ? -1 : 1
-      let nextPoints = initialPoints
-
-      let x = currentX
-      do {
-        x += direction
-        nextPoints += amphipod.type
-        if (map[`${x},0`] !== null) return
-      } while (x !== targetX)
-
-      nextPoints += amphipod.type * (home === homeSpots[0] ? 1 : 2)
-      const nextAmphipods: Array<Amphipod> = amphipods.map((a) =>
-        a === amphipod
-          ? {
-              ...a,
-              state: State.Done,
-              location: home,
-            }
-          : a,
-      )
-
-      const nextMap: Record<string, null | AmphipodType> = {
-        ...map,
-        [amphipod.location]: null,
-        [home]: amphipod.type,
-      }
-
-      result.push({
-        id: getGamePositionId(nextAmphipods),
-        prev: currentGame,
-        points: nextPoints,
-        amphipods: nextAmphipods,
-        map: nextMap,
-      })
-    }
-  })
-
-  return result
-}
+    return result
+  }
 
 const template = `#############
 #...........#
@@ -235,14 +214,110 @@ const print = (amphipods: Amphipod[]) => {
   console.log(res.map((line) => line.join("")).join("\n"))
 }
 
-const solution1 = () =>
-  graphDistinctSearch(
+const solution1 = () => {
+  const input = "CBBDDAAC"
+  const input2 = input.split("").map((c) => codeAmphipod[c])
+
+  const initialmap: Record<string, null | AmphipodType> = {}
+  for (let x = 0; x < 11; x++) initialmap[`${x},0`] = null
+  initialmap["2,1"] = input2[0]
+  initialmap["2,2"] = input2[1]
+  initialmap["4,1"] = input2[2]
+  initialmap["4,2"] = input2[3]
+  initialmap["6,1"] = input2[4]
+  initialmap["6,2"] = input2[5]
+  initialmap["8,1"] = input2[6]
+  initialmap["8,2"] = input2[7]
+
+  const initialAmphipods = [
+    { type: input2[0], state: State.Init, location: "2,1" },
+    { type: input2[1], state: State.Init, location: "2,2" },
+    { type: input2[2], state: State.Init, location: "4,1" },
+    { type: input2[3], state: State.Init, location: "4,2" },
+    { type: input2[4], state: State.Init, location: "6,1" },
+    { type: input2[5], state: State.Init, location: "6,2" },
+    { type: input2[6], state: State.Init, location: "8,1" },
+    { type: input2[7], state: State.Init, location: "8,2" },
+  ]
+
+  const initialGamePosition: GamePosition = {
+    id: getGamePositionId(initialAmphipods),
+    prev: null,
+    points: 0,
+    map: initialmap,
+    amphipods: initialAmphipods,
+  }
+
+  return graphDistinctSearch(
     initialGamePosition,
-    generateMoves,
+    generateMoves(2),
     (a, b) => b.points - a.points,
     (prev, current) => current.points < prev.points,
   ).points
+}
 
-const solution2 = (lines: string[]) => {}
+const solution2 = (lines: string[]) => {
+  const input = "CDDBBCBDDBAAAACC"
+  const input2 = input.split("").map((c) => codeAmphipod[c])
 
-export default [solution1]
+  const initialmap: Record<string, null | AmphipodType> = {}
+  for (let x = 0; x < 11; x++) initialmap[`${x},0`] = null
+  initialmap["2,1"] = input2[0]
+  initialmap["2,2"] = input2[1]
+  initialmap["2,3"] = input2[2]
+  initialmap["2,4"] = input2[3]
+
+  initialmap["4,1"] = input2[4]
+  initialmap["4,2"] = input2[5]
+  initialmap["4,3"] = input2[6]
+  initialmap["4,4"] = input2[7]
+
+  initialmap["6,1"] = input2[8]
+  initialmap["6,2"] = input2[9]
+  initialmap["6,3"] = input2[10]
+  initialmap["6,4"] = input2[11]
+
+  initialmap["8,1"] = input2[12]
+  initialmap["8,2"] = input2[13]
+  initialmap["8,3"] = input2[14]
+  initialmap["8,4"] = input2[15]
+
+  const initialAmphipods = [
+    { type: input2[0], state: State.Init, location: "2,1" },
+    { type: input2[1], state: State.Init, location: "2,2" },
+    { type: input2[2], state: State.Init, location: "2,3" },
+    { type: input2[3], state: State.Init, location: "2,4" },
+
+    { type: input2[4], state: State.Init, location: "4,1" },
+    { type: input2[5], state: State.Init, location: "4,2" },
+    { type: input2[6], state: State.Init, location: "4,3" },
+    { type: input2[7], state: State.Init, location: "4,4" },
+
+    { type: input2[8], state: State.Init, location: "6,1" },
+    { type: input2[9], state: State.Init, location: "6,2" },
+    { type: input2[10], state: State.Init, location: "6,3" },
+    { type: input2[11], state: State.Init, location: "6,4" },
+
+    { type: input2[12], state: State.Init, location: "8,1" },
+    { type: input2[13], state: State.Init, location: "8,2" },
+    { type: input2[14], state: State.Init, location: "8,3" },
+    { type: input2[15], state: State.Init, location: "8,4" },
+  ]
+
+  const initialGamePosition: GamePosition = {
+    id: getGamePositionId(initialAmphipods),
+    prev: null,
+    points: 0,
+    map: initialmap,
+    amphipods: initialAmphipods,
+  }
+
+  return graphDistinctSearch(
+    initialGamePosition,
+    generateMoves(4),
+    (a, b) => b.points - a.points,
+    (prev, current) => current.points < prev.points,
+  ).points
+}
+
+export default [solution1, solution2]
